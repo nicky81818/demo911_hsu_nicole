ab03e8f38393de9ac6414581d2eb6829bf4b6398
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-02-28 09:38:21 -0800

    finished step 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,3 @@
-function insertName() {
-    firebase.auth().onAuthStateChanged(user => { //will verify who is logged in
-        // Check if a user is signed in:
-        if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            console.log(user.displayName);  //print the user name in the browser console
-            user_Name = user.displayName;
-
-            //method #1:  insert with html only
-            //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-            //method #2:  insert using jquery
-            $("#name-goes-here").text(user_Name); //using jquery
-
-        } else {
-            // No user is signed in.
-        }
-    });
-}
-insertName(); //run the function
-
 // Function to read the quote of the day from Firestore "quotes" collection
 // Input param is the String representing the day of the week, aka, the document name
 function readQuote(day) {
@@ -34,3 +13,21 @@ function readQuote(day) {
       })
 }
 readQuote("tuesday");        //calling the function
+
+function insertNameFromFirestore(){
+     // to check if the user is logged in:
+     firebase.auth().onAuthStateChanged(user =>{
+         if (user){
+            console.log(user.uid); // let me to know who is the user that logged in to get the UID
+            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+            currentUser.get().then(userDoc=>{
+                //get the user name
+                var userName= userDoc.data().name;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); //jquery
+                document.getElementById("name-goes-here").innerText=userName;
+            })    
+        }    
+     })
+}
+insertNameFromFirestore() //run the function
